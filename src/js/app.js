d3b29838d283562afbbc67db0dcd28861eee7749
Edgar <e.gm27@outlook.com>
2025-07-16 17:09:01 -0700

    cambios en barra de navegacion

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,28 @@
 console.log("Desde JS");
+
+/*Como comentar varias lineas de codigo*/
+
+document.addEventListener('DOMContentLoaded', function() {
+  eventListeners();
+});
+
+function eventListeners() {
+  const mobileMenu = document.querySelector('.mobile-menu');
+  mobileMenu.addEventListener('click', menuresponsive);
+}
+
+function menuresponsive () {
+  const navegacion = document.querySelector('.navegacion');
+
+  /* Metodo para nuevos usuarios
+  if(navegacion.classList.contains('mostrar')) {
+    navegacion.classList.remove('mostrar');
+  } else {
+    navegacion.classList.add('mostrar');
+  }
+
+  */
+
+  //Metodo para ususarios un poco mas experimentados
+  navegacion.classList.toggle('mostrar');
+}
